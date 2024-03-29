8bd0116b79066e4c9779042fb25592942b536b86
Lena OUDJMAN <lo276838@is231077.intra.cea.fr>
2024-03-29 11:52:35 +0100

    + config

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+# paths
+General_path = '/volatile/Lena/Codes/Modèles/fastmri_tf_vs_torch/'
+
+Save_model_path = '/volatile/Lena/Codes/Modèles/fastmri_tf_vs_torch/fastmri_compare/models_train/'
+
+Data_brain_multicoil = '/volatile/FastMRI/brain_multicoil_train/multicoil_train/dataset/'
+Data_brain_MtoS ='/volatile/Lena/Data/MtoS/'
+Data_brain_singlecoil = '/volatile/Lena/Data/singleCoil/'
